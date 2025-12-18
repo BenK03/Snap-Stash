f6f4f2afcd3fcf41a23130aa5cb1a94b49457a4c
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-18 02:49:40 -0500

    Completed Vault table in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -16,6 +16,37 @@ create table Albums (
         foreign key (user_id) references User(user_id) -- the user_id in Albums must match a user_id in Users
         on delete cascade -- if a user is deleted, their albums are deleted too
         on update cascade -- if a user_id changes, update it in Albums too (which probably won't happen)
+    index idx_albums_user_created (user_id, created_at) -- pre-organize to make searching faster
+);
+
+create table Media (
+    media_id int unsigned auto_increment primary key,
+    user_id int unsigned not null,
+    storage_path varchar(255) not null,
+    media_type enum('photo', 'video') not null,
+    created_at timestamp not null default current_timestamp,
+    constraint fk_media_user
+        foreign key (user_id) references Users(user_id)
+        on delete cascade
+        on update cascade,
+    index idx_media_user_created(user_id, created_at)
+);
+
+create table Vault (
+    vault_id int unsigned auto_increment primary key,
+    user_id int unsigned not null,
+    media_id int unsigned not null,
+    created_at timestamp not null default current_timestamp,
+    constraint fk_vault_user
+        foreign key (user_id) references Users(user_id)
+        on delete cascade
+        on update cascade
+    constraint fk_vault_media
+        foreign key (media_id) references Media(media_id)
+        on delete cascade
+        on update cascade,
+    constraint uq_vault_media unique (media_id),
+    index idx_vault_user_created (user_id, created_at)
 );
 
 
