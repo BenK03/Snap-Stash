0006f999a9a15fb10a0070427d97f393a0dadf22
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-19 18:30:09 -0500

    Update routes.go
    Completed binding and username and password error checks for postRegister function.

--- backend/internal/auth/routes.go
+++ backend/internal/auth/routes.go
@@ -2,10 +2,29 @@ package auth
 
 import "github.com/gin-gonic/gin"
 
+
+// check if username and password are valid then store them in the DB
 func postRegister(c *gin.Context) {
-	// TODO
+	var req RegisterReq
+
+	// extract JSON body and put it into req variable
+	// if there are no errors then bind c to req.
+	// if there are errors, return
+	if err := c.ShouldBindJSON(&req); err != nil {
+		return
+	}
+
+	// check if username and password are empty
+	if req.Username == "" || req.Password == "" {
+		c.JSON(400, gin.H{"error": "Username and password required"})
+		return
+	}
+
+
 }
 
+
+// check if login credentials are in DB
 func postLogin(c *gin.Context) {
 	// TODO
 }
