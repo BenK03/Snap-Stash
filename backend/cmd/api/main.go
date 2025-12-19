dc64f1919443f1f591e74b5488da4fe32a0dc492
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-19 04:09:08 -0500

    Add MySQL integration to auth handlers
    also started Login func

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"snapstash/internal/auth"
 )
 
@@ -11,7 +13,8 @@ func main() {
 	api := router.Group("/api")
     authGroup := api.Group("/auth")
 
-	authHandler := &auth.Handler{}
+	db, _ := sql.Open("mysql", "user:password@tcp(localhost:3306)/snapstash")
+	authHandler := &auth.Handler{DB: db}
 	auth.RegisterRoutes(authGroup, authHandler)
 
 	router.Run("localhost:8080")
--- backend/internal/auth/handlers.go
+++ backend/internal/auth/handlers.go
@@ -3,11 +3,19 @@ package auth
 import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
+	"database/sql"
 )
 
-type Handler struct{}
+type Handler struct{
+	DB *sql.DB
+}
 
 type registerReq struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type loginReq struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
@@ -23,9 +31,21 @@ func (h *Handler) Register(c *gin.Context) {
 		return
 	}
 
+	// temporary
+	if h.DB == nil {
+		c.JSON(500, gin.H{"error": "db not initialized"})
+		return
+	}
+
 	// Hashes password using bcrypt
 	hash, _ := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
 	_ = hash
+
+	_, _ = h.DB.Exec(
+		"INSERT INTO Users (username, password_hash) VALUES (?, ?)",
+		req.Username,
+		string(hash),
+	)
 
 	// I will make DB calls later (use this for now)
 	c.JSON(201, gin.H{"status": "ok"})
@@ -33,5 +53,13 @@ func (h *Handler) Register(c *gin.Context) {
 }
 
 func (h *Handler) Login(c *gin.Context) {
-	// TODO: implement
+	var req loginReq
+	_ = c.ShouldBindJSON(&req)
+
+	// Check if username and password are provided
+	if req.Username == "" || req.Password == "" {
+		c.JSON(400, gin.H{"error": "username and password required"})
+		return
+	}
+
 }
