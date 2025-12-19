b6f03b190dfdf674e11760f81a25b80cb89e6256
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-19 17:51:46 -0500

    Deleted contents in routes.go and main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"snapstash/internal/auth"
 )
 
 func main() {
 	router := gin.Default()
-
-	api := router.Group("/api")
-    authGroup := api.Group("/auth")
-
-	db, _ := sql.Open("mysql", "root:rootpassword@tcp(localhost:3306)/snapstash")
-
-	authHandler := &auth.Handler{DB: db}
-	auth.RegisterRoutes(authGroup, authHandler)
-
 	router.Run("localhost:8080")
 }
--- backend/internal/auth/routes.go
+++ backend/internal/auth/routes.go
@@ -1,8 +1,5 @@
 package auth
 
-import "github.com/gin-gonic/gin"
+// import "github.com/gin-gonic/gin"
 
-func RegisterRoutes(rg *gin.RouterGroup, h *Handler) {
-	rg.POST("/register", h.Register)
-	rg.POST("/login", h.Login)
-}
+
