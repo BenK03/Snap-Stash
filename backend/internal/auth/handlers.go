a3e6d40e94e87dc84c49c7c991cd0dc1c475f530
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-19 04:00:53 -0500

    Completed Register Handler
    Minimally completed register handler. Will connect to DB later.

--- backend/internal/auth/handlers.go
+++ backend/internal/auth/handlers.go
@@ -1,6 +1,9 @@
 package auth
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type Handler struct{}
 
@@ -9,9 +12,24 @@ type registerReq struct {
 	Password string `json:"password"`
 }
 
-
+// accepts JSON body with username and password
 func (h *Handler) Register(c *gin.Context) {
-	// TODO: implement
+	var req registerReq
+	_ = c.ShouldBindJSON(&req)
+
+	// Check if username and password are provided
+	if req.Username == "" || req.Password == "" {
+		c.JSON(400, gin.H{"error": "username and password required"})
+		return
+	}
+
+	// Hashes password using bcrypt
+	hash, _ := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	_ = hash
+
+	// I will make DB calls later (use this for now)
+	c.JSON(201, gin.H{"status": "ok"})
+
 }
 
 func (h *Handler) Login(c *gin.Context) {
