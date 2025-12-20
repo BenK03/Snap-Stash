4c517b2b44f8f5b2bb69787450649492c77293a6
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-20 01:22:33 -0500

    Update routes.go
    In media folder. Completed pulling user id

--- backend/internal/media/routes.go
+++ backend/internal/media/routes.go
@@ -2,11 +2,29 @@ package media
 
 import (
 	"database/sql"
+	"strconv"
+	"strings"
 	"github.com/gin-gonic/gin"
 	snapminio "snapstash/internal/storage/minio"
 )
 
 func PostUpload(c *gin.Context, db *sql.DB, minioClient *snapminio.Client) {
-	// TODO
-	c.JSON(501, gin.H{"error": "not implemented"})
+	_ = db
+	_ = minioClient
+
+	// get user id
+	userIDRaw := strings.TrimSpace(c.GetHeader("X-User-ID"))
+	if userIDRaw == "" { // if user id missing
+		c.JSON(400, gin.H{"error": "missing X-User-ID header"})
+		return
+	}
+
+	userID, err := strconv.Atoi(userIDRaw) // convert the id to a integer
+	if err != nil || userID <= 0 { // if non integer or equal to 0 or less return error
+		c.JSON(400, gin.H{"error": "invalid X-User-ID header"})
+		return
+	}
+
+
+
 }
