ac5e8e91240d9379ce281f1420a2d2a23a793e1c
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-20 01:39:46 -0500

    Update routes.go
    More error handling.

--- backend/internal/media/routes.go
+++ backend/internal/media/routes.go
@@ -25,6 +25,33 @@ func PostUpload(c *gin.Context, db *sql.DB, minioClient *snapminio.Client) {
 		return
 	}
 
+	// pull file
+	fh, err := c.FormFile("file")
+	if err != nil { // if not nil then empty so show error
+		c.JSON(400, gin.H{"error": "missing file (field name must be 'file')"})
+		return
+	}
+
+	// show error if file size <= 0
+	if fh.Size <= 0 {
+		c.JSON(400, gin.H{"error": "empty file"})
+		return
+	}
+
+	// reject uploads that aren't images or videos
+	mimeType := strings.TrimSpace(fh.Header.Get("Content-Type"))
+	if !strings.HasPrefix(mimeType, "image/") && !strings.HasPrefix(mimeType, "video/") {
+		c.JSON(415, gin.H{"error": "only image/* and video/* allowed"})
+		return
+	}
+
+	// check if we can open the file
+	file, err := fh.Open()
+	if err != nil {
+		c.JSON(500, gin.H{"error": "failed to open uploaded file"})
+		return
+	}
+	defer file.Close()
 
 
 }
