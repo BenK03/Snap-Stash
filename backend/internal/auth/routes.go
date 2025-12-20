013f02ed987fba77e6d7f886e1df44dc06e01eb8
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-19 19:30:32 -0500

    Update routes.go
    completed binding, empty username/password check, and getting corresponding password from username in db.

--- backend/internal/auth/routes.go
+++ backend/internal/auth/routes.go
@@ -41,16 +41,46 @@ func postRegister(c *gin.Context, db *sql.DB) {
 		c.JSON(500, gin.H{"error": "db insert failed"})
 		return
 	}
-	c.JSON(201, gin.H{"status": "ok"})
-
-
-
+	c.JSON(201, gin.H{"status": "ok"}) // send to frontend to proceed
 
 }
 
 
 // check if login credentials are in DB
 func postLogin(c *gin.Context, db *sql.DB) {
-	// TODO
+	var req LoginReq
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": "invalid json"})
+		return
+	}
+
+	if req.Username == "" || req.Password == "" {
+		c.JSON(400, gin.H{"error": "Username and password required"})
+		return
+	}
+
+	// if all is good find username and corresponding password hash from DB
+	var storedHash string
+	err := db.QueryRow(
+		"select password_hash from Users where username = ?",
+		req.Username,
+	).Scan(&storedHash)
+
+	// handle user not found
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(401, gin.H{"error": "invalid credentials"})
+			return
+		}
+
+		c.JSON(500, gin.H{"error": "db error"})
+		return
+	}
+
+
+
+
+
 }
 
