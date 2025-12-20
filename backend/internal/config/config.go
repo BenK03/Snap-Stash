c9a40b53a0ca2098c2f5d827e3245502d13d192b
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-19 22:17:07 -0500

    Update config.go
    Added two structs.

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,1 +1,20 @@
 package config
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+// struct that holds everything we need to talk to the minio
+type MinIOConfig struct {
+	Endpoint  string
+	AccessKey string
+	SecretKey string
+	Bucket    string
+	UseSSL    bool
+}
+
+type Config struct {
+	MinIO MinIOConfig
+}
