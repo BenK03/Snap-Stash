f1e1404c33292b9187e01d035b5bf45daff07607
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2025-12-21 00:35:50 -0500

    Starting GetMedia Endpoint

--- backend/internal/media/routes.go
+++ backend/internal/media/routes.go
@@ -118,3 +118,19 @@ func PostUpload(c *gin.Context, db *sql.DB, minioClient *snapminio.Client) {
 	})
 
 }
+
+func GetMedia(c *gin.Context, db *sql.DB) {
+	
+	// get user id/check if it is valid
+	userIDRaw := strings.TrimSpace(c.GetHeader("X-User-ID"))
+	if userIDRaw == "" { // if user id missing
+		c.JSON(400, gin.H{"error": "missing X-User-ID header"})
+		return
+	}
+
+	userID, err := strconv.Atoi(userIDRaw) // convert the id to a integer
+	if err != nil || userID <= 0 {         // if non integer or equal to 0 or less return error
+		c.JSON(400, gin.H{"error": "invalid X-User-ID header"})
+		return
+	}
+}
